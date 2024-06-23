9689e6247ca59a7d8214362f304e2f3fd8586080
=Biswaranjan <biswaranjanb627@gmail.com>
2024-06-24 01:58:36 +0530

    24/06/2024

--- Html/Book Management App/script.js
+++ Html/Book Management App/script.js
@@ -1,4 +1,4 @@
-/*let arrBooks = [];
+let arrBooks = [];
 function add() {
     let id = document.getElementById("bookid").value;
     let name = document.getElementById("bookname").value;
@@ -27,10 +27,8 @@ function add() {
 
 function BindTable(){
     let books = JSON.parse(localStorage.getItem("books"));
-    alert(books);
   let data = "";
   if (books != null && books.length > 0) {
-    
     for (let i = 0; i < books.length; i++) {
       
    let data='<tr>'
@@ -51,7 +49,7 @@ function BindTable(){
     data += "<tr><td colspan='6'><h4 style='text-align:center'>No Record Found</h4></td></tr>";
   }
 
-  document.getElementById("tabledata").innerHTML = data;        
+  document.getElementById("tabledata").innerHTML =data;        
 }
 
  function validate(id,name,author,price,image) {
@@ -77,82 +75,5 @@ function BindTable(){
         return false;
       }
       return true;
-    }
-      */
-    let arrBooks = [];
-
-    function add() {
-        let id = document.getElementById("bookid").value;
-        let name = document.getElementById("bookname").value;
-        let author = document.getElementById("bookauthor").value;
-        let price = document.getElementById("bookprice").value;
-        let image = document.getElementById("bookimage").value;
-    
-        if (validate(id, name, author, price, image)) {
-            alert("Validation ok");
-            let book = {
-                Id: id,
-                Name: name,
-                Author: author,
-                Price: price,
-                Image: image
-            };
-    
-            arrBooks = JSON.parse(localStorage.getItem("books"));
-            arrBooks = arrBooks == null ? [] : arrBooks;
-            arrBooks.push(book);
-            localStorage.setItem("books", JSON.stringify(arrBooks));
-    
-            BindTable();
-        }
-    }
-    
-    function BindTable() {
-        let books = JSON.parse(localStorage.getItem("books"));
-        alert(JSON.stringify(books));
-        let data = "";
-        if (books != null && books.length > 0) {
-            for (let i = 0; i < books.length; i++) {
-                data += '<tr>';
-                data += `<td>${i + 1}</td>`;
-                data += `<td>${books[i].Id}</td>`;
-                data += `<td>${books[i].Name}</td>`;
-                data += `<td>${books[i].Author}</td>`;
-                data += `<td>${books[i].Price}</td>`;
-                data += `<td><img src="${books[i].Image}" alt="Book Image" width="50" height="50"></td>`;
-                data += `<td><button>Edit</button></td>`;
-                data += `<td><button>Delete</button></td>`;
-                data += '</tr>';
-            }
-        } else {
-            data += "<tr><td colspan='8'><h4 style='text-align:center'>No Record Found</h4></td></tr>";
-        }
-    
-        document.getElementById("tabledata").innerHTML = data;
-    }
-    
-    function validate(id, name, author, price, image) {
-        if (id == "") {
-            alert("Please Enter Book Id");
-            document.getElementById("bookid").focus();
-            return false;
-        } else if (name == "") {
-            alert("Please Enter Book Name");
-            document.getElementById("bookname").focus();
-            return false;
-        } else if (author == "") {
-            alert("Please Enter Book Author Name");
-            document.getElementById("bookauthor").focus();
-            return false;
-        } else if (price == "") {
-            alert("Please Enter Book Price");
-            document.getElementById("bookprice").focus();
-            return false;
-        } else if (image == "") {
-            alert("Please Enter Book Image Url");
-            document.getElementById("bookimage").focus();
-            return false;
-        }
-        return true;
     }
     
